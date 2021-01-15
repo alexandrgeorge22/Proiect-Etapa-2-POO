ae4eabf640d92df8b34523fef013d2a38ee04231
alexandrgeorge22 <you@example.com>
2021-01-15 19:45:05 +0200

    Spart main in mai multe functii specifice pentru distribuitori si consumatori

--- src/fileio/Consumer.java
+++ src/fileio/Consumer.java
@@ -1,5 +1,7 @@
 package fileio;
 
+import java.util.List;
+
 public final class Consumer {
     private Integer id;
     private Integer budget;
@@ -54,8 +56,15 @@ public final class Consumer {
         return distributor;
     }
 
-    public void setDistributor(final Integer distributor) {
-        this.distributor = distributor;
+    public void setDistributor(final Distributor distributor) {
+
+        if (distributor == null) {
+            this.distributor = null;
+        } else {
+            this.setActualContractLength(distributor.getContractLength());
+            this.setContractPrice(distributor.getContractPrice());
+            this.distributor = distributor.getId();
+        }
     }
 
     public Integer getId() {
@@ -80,5 +89,83 @@ public final class Consumer {
 
     public void setMonthlyIncome(final Integer monthlyIncome) {
         this.monthlyIncome = monthlyIncome;
+    }
+
+    public void checkContract(final List<Distributor> distributorsData,
+                              final List<Distributor> updateddistributors) {
+        if (this.getActualContractLength() == 0) {
+            if (this.getPenalty() != null) {
+                Integer penalty = Math.toIntExact(Math.round(Math.floor(1.2 * this.
+                        getPenalty())));
+                if (this.getBudget() >= penalty + this.getContractPrice()) {
+                    this.setBudget(this.getBudget() - penalty - this.
+                            getContractPrice());
+                    distributorsData.get(this.getDistributor()).
+                            setBudget(distributorsData.get(this.
+                                    getDistributor()).getBudget() + penalty);
+                    distributorsData.get(this.getDistributor()).
+                            setContractedConsumers(distributorsData.
+                                    get(this.getDistributor()).
+                                    getContractedConsumers() - 1);
+                    this.setPenalty(null);
+                    this.setDistributor(updateddistributors.get(0));
+                    distributorsData.get(this.getDistributor()).
+                            setContractedConsumers(distributorsData.
+                                    get(this.getDistributor()).
+                                    getContractedConsumers() + 1);
+                } else {
+                    this.setBankrupt(true);
+                }
+            } else {
+                if (this.getDistributor() != null) {
+                    distributorsData.get(this.getDistributor()).
+                            setContractedConsumers(distributorsData.
+                                    get(this.getDistributor()).
+                                    getContractedConsumers() - 1);
+                }
+                this.setDistributor(updateddistributors.get(0));
+                distributorsData.get(this.getDistributor()).
+                        setContractedConsumers(distributorsData.get(this.getDistributor()).
+                                getContractedConsumers() + 1);
+            }
+        }
+    }
+
+    public void payContract(List<Distributor> distributorsData) {
+        if (this.getBudget() >= this.getContractPrice()) {
+            if (this.getPenalty() != null) {
+                Integer penalty = Math.toIntExact(Math.round(Math.
+                        floor(1.2 * this.getPenalty())));
+                if (this.getBudget() >= this.getContractPrice() + penalty) {
+                    this.setBudget(this.getBudget() - this.
+                            getContractPrice() - penalty);
+                    this.setPenalty(null);
+                    distributorsData.get(this.
+                            getDistributor()).setBudget(
+                            distributorsData.get(this.
+                                    getDistributor()).getBudget()
+                                    + this.getContractPrice() + penalty);
+                    this.setActualContractLength(this.
+                            getActualContractLength() - 1);
+                } else {
+                    this.setBankrupt(true);
+                }
+            } else {
+                this.setBudget(this.getBudget() - this.
+                        getContractPrice());
+                distributorsData.get(this.
+                        getDistributor()).setBudget(distributorsData.
+                        get(this.getDistributor()).getBudget()
+                        + this.getContractPrice());
+                this.setActualContractLength(this.
+                        getActualContractLength() - 1);
+            }
+        } else if (this.getPenalty() == null) {
+            this.setPenalty(this.getContractPrice());
+            this.setActualContractLength(this.
+                    getActualContractLength() - 1);
+        } else {
+            this.setBankrupt(true);
+        }
     }
 }
